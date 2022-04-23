23dec569b497e2dd3e69e70c2c2322a022c9c5b6
GB-Tomas <82116700+GB-Tomas@users.noreply.github.com>
2022-04-22 22:25:44 -0300

    TP1 v1.0

---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main (void) {
+
+	int caracter;
+
+	caracter = getchar();
+
+	while (caracter != EOF) {
+		if(islower(caracter)) {
+			putchar(toupper(caracter));
+		}
+		else if (isupper(caracter)) {
+			putchar(tolower(caracter));
+		}
+		else if (!isdigit(caracter)){
+			putchar(caracter);
+		}
+		caracter = getchar();
+	}
+
+}
